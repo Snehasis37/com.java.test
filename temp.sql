f01148470b26d3510325fffbf5af6ef95158ace7
Snehasis37 <30543780+Snehasis37@users.noreply.github.com>
2017-07-29 11:19:36 +0530

    sql
    sql

---
+++ temp.sql
@@ -0,0 +1,10 @@
+ create or replace function addition( a IN number,b IN number, c OUT number) 
+ return number AS
+ num1 number:= a;
+ num2 number:= b;
+ begin
+ c := num1 +num2;
+ return c;
+ end;
+ /
+
